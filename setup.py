1a6e486f0a1b56cd29245d79ec115e70cb05b8c2
Shaik abdul khadar <146209562+Shaik31@users.noreply.github.com>
2024-08-04 16:49:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import find_packages, setup
+'''from setuptools import find_packages, setup
 from typing import List
 HYPEN_E_DOT='-e .'
 
 def get_requirements(file_path:str)->List[str]:
     '''
-    this function will return the list of requirements
+    #this function will return the list of requirements
     '''
     requirements=[]
     with open(file_path) as file_obj:
@@ -20,4 +20,21 @@ setup(
     author="Shaik Abdul Khadar",
     packages=find_packages(),
     install_requires = get_requirements('requirements.txt')
+)'''
+
+from setuptools import setup, find_packages
+
+setup(
+    name='movie_recommendation_engine',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.21.0',
+        'pandas>=1.3.0',
+        'scikit-learn>=0.24.0',
+        'streamlit>=0.87.0',
+        'pickle>=4.0'
+        # Add other dependencies here
+    ],
 )
+
